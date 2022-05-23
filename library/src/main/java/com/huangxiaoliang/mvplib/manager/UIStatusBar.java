658be4b25463aa164f81eb9353a4ed6ec3b84672
HHotHeart <2550754753@qq.com>
2022-05-23 19:57:36 +0800

    修复状态栏字体颜色问题

--- library/src/main/java/com/huangxiaoliang/mvplib/manager/UIStatusBar.java
+++ library/src/main/java/com/huangxiaoliang/mvplib/manager/UIStatusBar.java
@@ -186,7 +186,7 @@ public class UIStatusBar {
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE ^ View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         } else {
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
